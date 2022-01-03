b00388ba45559ddc9faca4021ed0531bc93d8064
Gusti Ridhoni Syahputera <syahputera.doni@gmail.com>
2022-01-03 18:27:22 +0800

    Convert AuthenticationRepositoryPostgres to Typescript

--- src/Domains/authentications/AuthenticationRepository.ts
+++ src/Domains/authentications/AuthenticationRepository.ts
@@ -1,5 +1,5 @@
 export default interface AuthenticationRepository {
   addToken: (token: string) => Promise<any>
-  checkAvailabilityToken: (token: string) => Promise<any>
+  checkAvailabilityToken: (token: string) => Promise<void>
   deleteToken: (token: string) => Promise<any>
 }
