33d2647721c9b1e7c6810a9c748ce6bf0fd88710
Gusti Ridhoni Syahputera <syahputera.doni@gmail.com>
2022-01-03 15:48:13 +0800

    Convert DeleteAuthenticationUseCase to Typescript

---
+++ src/Applications/use_case/_test/DeleteAuthenticationUseCase.test.ts
@@ -0,0 +1,53 @@
+import AuthenticationRepository from '../../../Domains/authentications/AuthenticationRepository'
+import DeleteAuthenticationUseCase from '../DeleteAuthenticationUseCase'
+
+const getAuthenticationRepositoryMock = (): AuthenticationRepository => ({
+  addToken: jest.fn().mockImplementation(() => {}),
+  checkAvailabilityToken: jest.fn().mockImplementation(async () => await Promise.resolve()),
+  deleteToken: jest.fn().mockImplementation(async () => await Promise.resolve())
+})
+
+describe('DeleteAuthenticationUseCase', () => {
+  it('should throw error if use case payload not contain refresh token', async () => {
+    // Arrange
+    const useCasePayload = {}
+
+    const deleteAuthenticationUseCase = new DeleteAuthenticationUseCase(getAuthenticationRepositoryMock())
+
+    // Action & Assert
+    await expect(deleteAuthenticationUseCase.execute(useCasePayload))
+      .rejects
+      .toThrowError('DELETE_AUTHENTICATION_USE_CASE.NOT_CONTAIN_REFRESH_TOKEN')
+  })
+
+  it('should throw error if refresh token not string', async () => {
+    // Arrange
+    const useCasePayload = {
+      refreshToken: 123
+    }
+    const deleteAuthenticationUseCase = new DeleteAuthenticationUseCase(getAuthenticationRepositoryMock())
+
+    // Action & Assert
+    await expect(deleteAuthenticationUseCase.execute(useCasePayload))
+      .rejects
+      .toThrowError('DELETE_AUTHENTICATION_USE_CASE.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION')
+  })
+
+  it('should orchestrating the delete authentication action correctly', async () => {
+    // Arrange
+    const useCasePayload = {
+      refreshToken: 'refreshToken'
+    }
+    const mockAuthenticationRepository = getAuthenticationRepositoryMock()
+    const deleteAuthenticationUseCase = new DeleteAuthenticationUseCase(mockAuthenticationRepository)
+
+    // Act
+    await deleteAuthenticationUseCase.execute(useCasePayload)
+
+    // Assert
+    expect(mockAuthenticationRepository.checkAvailabilityToken)
+      .toHaveBeenCalledWith(useCasePayload.refreshToken)
+    expect(mockAuthenticationRepository.deleteToken)
+      .toHaveBeenCalledWith(useCasePayload.refreshToken)
+  })
+})
