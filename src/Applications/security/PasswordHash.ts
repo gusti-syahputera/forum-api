260227c1937e5d1587f27950435cba20b147bdae
Gusti Ridhoni Syahputera <syahputera.doni@gmail.com>
2022-01-03 17:34:17 +0800

    Convert BcryptPasswordHash to Typescript

--- src/Applications/security/PasswordHash.ts
+++ src/Applications/security/PasswordHash.ts
@@ -1,4 +1,4 @@
 export default interface PasswordHash {
   hash: (password: string) => Promise<string>
-  comparePassword: (plain: string, encrypted: string) => Promise<boolean>
+  comparePassword: (plain: string, encrypted: string) => Promise<void>
 }
