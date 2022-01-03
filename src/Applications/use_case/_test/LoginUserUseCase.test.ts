edf9768906e1b2e973840620325a081c8eaa616e
Gusti Ridhoni Syahputera <syahputera.doni@gmail.com>
2022-01-03 17:03:36 +0800

    Convert LoginUseCase to Typescript

---
+++ src/Applications/use_case/_test/LoginUserUseCase.test.ts
@@ -0,0 +1,69 @@
+import UserRepository from '../../../Domains/users/UserRepository'
+import AuthenticationRepository from '../../../Domains/authentications/AuthenticationRepository'
+import AuthenticationTokenManager from '../../security/AuthenticationTokenManager'
+import PasswordHash from '../../security/PasswordHash'
+import LoginUserUseCase from '../LoginUserUseCase'
+import NewAuth from '../../../Domains/authentications/entities/NewAuth'
+
+describe('GetAuthenticationUseCase', () => {
+  it('should orchestrating the get authentication action correctly', async () => {
+    // Arrange
+    const useCasePayload = {
+      username: 'dicoding',
+      password: 'secret'
+    }
+    const expectedAuthentication = new NewAuth({
+      accessToken: 'access_token',
+      refreshToken: 'refresh_token'
+    })
+
+    // Mocked dependencies
+    const mockUserRepository: UserRepository = {
+      addUser: jest.fn().mockImplementation(() => {}),
+      getIdByUsername: jest.fn().mockImplementation(async () => 'user-123'),
+      getPasswordByUsername: jest.fn().mockImplementation(async () => 'encrypted_password'),
+      verifyAvailableUsername: jest.fn().mockImplementation(() => {})
+    }
+    const mockAuthenticationRepository: AuthenticationRepository = {
+      addToken: jest.fn().mockImplementation(async () => await Promise.resolve()),
+      checkAvailabilityToken: jest.fn().mockImplementation(() => {}),
+      deleteToken: jest.fn().mockImplementation(() => {})
+    }
+    const mockAuthenticationTokenManager: AuthenticationTokenManager = {
+      createAccessToken: jest.fn().mockImplementation(async () => expectedAuthentication.accessToken),
+      createRefreshToken: jest.fn().mockImplementation(async () => expectedAuthentication.refreshToken),
+      decodePayload: jest.fn().mockImplementation(() => {}),
+      verifyRefreshToken: jest.fn().mockImplementation(() => {})
+    }
+    const mockPasswordHash: PasswordHash = {
+      comparePassword: jest.fn().mockImplementation(async () => await Promise.resolve()),
+      hash: jest.fn().mockImplementation(async () => await Promise.resolve())
+    }
+
+    // create use case instance
+    const loginUserUseCase = new LoginUserUseCase(
+      mockUserRepository,
+      mockAuthenticationRepository,
+      mockAuthenticationTokenManager,
+      mockPasswordHash
+    )
+
+    // Action
+    const actualAuthentication = await loginUserUseCase.execute(useCasePayload)
+
+    // Assert
+    expect(actualAuthentication).toEqual(expectedAuthentication)
+    expect(mockUserRepository.getPasswordByUsername)
+      .toBeCalledWith('dicoding')
+    expect(mockPasswordHash.comparePassword)
+      .toBeCalledWith('secret', 'encrypted_password')
+    expect(mockUserRepository.getIdByUsername)
+      .toBeCalledWith('dicoding')
+    expect(mockAuthenticationTokenManager.createAccessToken)
+      .toBeCalledWith({ username: 'dicoding', id: 'user-123' })
+    expect(mockAuthenticationTokenManager.createRefreshToken)
+      .toBeCalledWith({ username: 'dicoding', id: 'user-123' })
+    expect(mockAuthenticationRepository.addToken)
+      .toBeCalledWith(expectedAuthentication.refreshToken)
+  })
+})
