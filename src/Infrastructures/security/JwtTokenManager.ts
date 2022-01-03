ead526021d6dc98cb0de1e4e35890e60918f7f33
Gusti Ridhoni Syahputera <syahputera.doni@gmail.com>
2022-01-03 17:57:42 +0800

    Convert JwtTokenManager to Typescript

---
+++ src/Infrastructures/security/JwtTokenManager.ts
@@ -0,0 +1,43 @@
+import 'reflect-metadata'
+
+import { inject, injectable } from 'tsyringe'
+
+import AuthenticationTokenManager from '../../Applications/security/AuthenticationTokenManager'
+import InvariantError from '../../Commons/exceptions/InvariantError'
+
+export interface IJwtHelper {
+  generate: (payload, secret, options?) => string
+  decode: (token: string, options?) => any | never
+  verify: (artifacts, secret, options?) => void | never
+}
+
+@injectable()
+export default class JwtTokenManager implements AuthenticationTokenManager {
+  private readonly jwt: IJwtHelper;
+
+  constructor (@inject('jwt') jwt: IJwtHelper) {
+    this.jwt = jwt
+  }
+
+  async createAccessToken (payload) {
+    return this.jwt.generate(payload, process.env.ACCESS_TOKEN_KEY)
+  }
+
+  async createRefreshToken (payload) {
+    return this.jwt.generate(payload, process.env.REFRESH_TOKEN_KEY)
+  }
+
+  async verifyRefreshToken (token) {
+    try {
+      const artifacts = this.jwt.decode(token)
+      this.jwt.verify(artifacts, process.env.REFRESH_TOKEN_KEY)
+    } catch (error) {
+      throw new InvariantError('refresh token tidak valid')
+    }
+  }
+
+  async decodePayload (token) {
+    const artifacts = this.jwt.decode(token)
+    return artifacts.decoded.payload
+  }
+}
