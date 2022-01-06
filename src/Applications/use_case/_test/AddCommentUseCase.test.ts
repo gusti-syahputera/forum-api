9310ab572bc9bcaa225641c4830601319405a7ec
Gusti Ridhoni Syahputera <syahputera.doni@gmail.com>
2022-01-06 18:46:06 +0800

    Refactor code

--- src/Applications/use_case/_test/AddCommentUseCase.test.ts
+++ src/Applications/use_case/_test/AddCommentUseCase.test.ts
@@ -3,7 +3,7 @@ import { createMock } from 'ts-auto-mock'
 import { method, On } from 'ts-auto-mock/extension'
 
 import AddCommentUseCase, { Payload } from '../AddCommentUseCase'
-import { AddedComment } from '../../../Domains/comments/entities'
+import { AddedComment, NewComment } from '../../../Domains/comments/entities'
 import CommentRepository from '../../../Domains/comments/CommentRepository'
 
 describe('AddCommentUseCase', () => {
@@ -15,23 +15,29 @@ describe('AddCommentUseCase', () => {
       owner: `user-${faker.datatype.uuid()}`
     }
 
-    // Arrange UseCase's dependencies
-    const expectedAddedComment = new AddedComment({
-      id: `thread-${faker.datatype.uuid()}`,
+    // Arrange doubles
+    const newComment = new NewComment(useCasePayload)
+    const addedComment = new AddedComment({
+      id: `comment-${faker.datatype.uuid()}`,
       content: useCasePayload.content,
       owner: useCasePayload.owner
     })
     const commentRepository = createMock<CommentRepository>()
-    const spyAddCommentMethod: jest.Mock = On(commentRepository)
-      .get(method(method => method.addComment))
-      .mockResolvedValue(expectedAddedComment)
+    const commentRepositoryMocks = {
+      addComment: On(commentRepository)
+        .get(method(method => method.addComment))
+        .mockResolvedValue(addedComment)
+    }
+
+    // Arrange
+    const expectedAddedComment = Object.assign(addedComment)
 
     // Action
     const useCase = new AddCommentUseCase(commentRepository)
-    const addedComment = await useCase.execute(useCasePayload)
+    const promise = useCase.execute(useCasePayload)
 
     // Assert
-    expect(addedComment).toStrictEqual(expectedAddedComment)
-    expect(spyAddCommentMethod).toBeCalledWith(useCasePayload)
+    await expect(promise).resolves.toStrictEqual(expectedAddedComment)
+    expect(commentRepositoryMocks.addComment).toBeCalledWith(newComment)
   })
 })
