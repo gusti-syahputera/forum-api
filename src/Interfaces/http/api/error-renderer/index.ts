b2da92e87f12f5c13c149705227001f1bf95bd4c
Gusti Ridhoni Syahputera <syahputera.doni@gmail.com>
2022-01-06 14:21:55 +0800

    Move handler logic to package's root

--- src/Interfaces/http/api/error-renderer/index.ts
+++ src/Interfaces/http/api/error-renderer/index.ts
@@ -1,7 +1,13 @@
-import { Plugin, Server, ServerRegisterPluginObject as PluginObject } from '@hapi/hapi'
+import {
+  Plugin,
+  Request,
+  ResponseToolkit,
+  Server,
+  ServerRegisterPluginObject as PluginObject
+} from '@hapi/hapi'
 
-import ErrorRenderer from './handler'
 import ResponseRenderer from '../../ResponseRenderer'
+import { ClientError, DomainErrorTranslator } from '../../../../Commons/exceptions'
 
 export interface Options {
   renderer: ResponseRenderer
@@ -10,8 +16,7 @@ export interface Options {
 export const errorRenderer: Plugin<Options> = {
   name: 'error-renderer',
   register: async (server, { renderer }) => {
-    const handler = new ErrorRenderer(renderer)
-    server.ext('onPreResponse', handler.onPreResponseHandler)
+    server.ext('onPreResponse', makeHandler(renderer))
   }
 }
 
@@ -24,3 +29,29 @@ const register = async (server: Server, renderer: ResponseRenderer): Promise<voi
 }
 
 export default register
+
+const makeHandler = (renderer: ResponseRenderer) => (request: Request, h: ResponseToolkit) => {
+  // mendapatkan konteks response dari request
+  const { response } = request
+
+  if (response instanceof Error) {
+    // bila response tersebut error, tangani sesuai kebutuhan
+    const translatedError = DomainErrorTranslator.translate(response)
+
+    // penanganan client error secara internal.
+    if (translatedError instanceof ClientError) {
+      return renderer.fail(h, translatedError.message, translatedError.statusCode)
+    }
+
+    // mempertahankan penanganan client error oleh hapi secara native, seperti 404, etc.
+    if (!translatedError.isServer) {
+      return h.continue
+    }
+
+    // penanganan server error sesuai kebutuhan
+    return renderer.internalError(h)
+  }
+
+  // jika bukan error, lanjutkan dengan response sebelumnya (tanpa terintervensi)
+  return h.continue
+}
