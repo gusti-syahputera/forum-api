c9e7515b738818755042b2a756db1c7d75fe3a45
Gusti Ridhoni Syahputera <syahputera.doni@gmail.com>
2022-01-06 14:01:19 +0800

    Use ACCESS_TOKEN_AGE variable when provided
    ACCCESS_TOKEN_AGE was a typo from the starter project.

--- src/Interfaces/http/jwtAuthStrategy.ts
+++ src/Interfaces/http/jwtAuthStrategy.ts
@@ -4,13 +4,18 @@ import { Server, ServerAuthSchemeOptions } from '@hapi/hapi'
 export default async (server: Server, authStrategyName: string): Promise<string> => {
   await server.register({ plugin: Jwt })
 
+  // Pada proyek starter, ditetapkan sebuah variabel yang typo (ACCCESS_TOKEN_AGE) dalam file .env.
+  // Sedangkan reviewer submisi mungkin saja menggunakan variabel yang benar (ACCESS_TOKEN_AGE).
+  // Oleh karena itu, digunakanlah percabangan untuk menetapkan nilai maxAgeSec di bawah.
+  // Statement berikut diabaikan karena hanya salah satu cabang yang akan terkover pada pengujian.
+  /* istanbul ignore next */
   const options: ServerAuthSchemeOptions = {
     keys: process.env.ACCESS_TOKEN_KEY,
     verify: {
       aud: false,
       iss: false,
       sub: false,
-      maxAgeSec: process.env.ACCCESS_TOKEN_AGE ?? 15
+      maxAgeSec: process.env.ACCCESS_TOKEN_AGE ?? process.env.ACCESS_TOKEN_AGE
     },
     validate: (artifacts) => ({
       isValid: true,
