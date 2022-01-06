19c8967d3737fd4c818900592f9feb69ff0f6969
Gusti Ridhoni Syahputera <syahputera.doni@gmail.com>
2022-01-06 16:23:50 +0800

    Fix addComment function

--- src/Commons/tests/CommentsTableTestHelper.ts
+++ src/Commons/tests/CommentsTableTestHelper.ts
@@ -5,15 +5,16 @@ import pool from '../../Infrastructures/database/postgres/pool'
 
 export default {
   async addComment ({
-    id = `thread-${faker.datatype.uuid()}`,
-    content = faker.lorem.paragraphs(),
+    id = `comment-${faker.datatype.uuid()}`,
+    thread_id = `thread-${faker.datatype.uuid()}`,
     owner = `user-${faker.datatype.uuid()}`,
+    content = faker.lorem.paragraphs(),
     date = faker.datatype.datetime().toISOString(),
     deleted_at = faker.datatype.datetime().toISOString()
   }) {
     await pool.query({
-      text: 'INSERT INTO comments VALUES($1, $2, $3, $4, $5)',
-      values: [id, content, owner, date, deleted_at]
+      text: 'INSERT INTO comments VALUES($1, $2, $3, $4, $5, $6)',
+      values: [id, thread_id, owner, content, date, deleted_at]
     })
 
     return { id, content, owner, date, deleted_at }
