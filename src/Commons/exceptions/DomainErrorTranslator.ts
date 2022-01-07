fd1ef28bd22d35d6049d7e68e1305ca74ebacd81
Gusti Ridhoni Syahputera <syahputera.doni@gmail.com>
2022-01-07 14:14:32 +0800

    Add error message translations for Reply entities

--- src/Commons/exceptions/DomainErrorTranslator.ts
+++ src/Commons/exceptions/DomainErrorTranslator.ts
@@ -27,7 +27,11 @@ export default class DomainErrorTranslator {
     'DELETE_COMMENT.THREAD_NOT_FOUND': new NotFoundError('tidak dapat menghapus comment karena thread tidak ada'),
     'DELETE_COMMENT.COMMENT_DOES_NOT_BELONG_TO_THREAD': new InvariantError('tidak dapat menghapus comment karena comment tidak ada pada thread'),
     'DELETE_COMMENT.COMMENT_NOT_FOUND': new NotFoundError('tidak dapat menghapus comment karena comment tidak ada'),
-    'DELETE_COMMENT.USER_IS_NOT_OWNER': new AuthorizationError('tidak dapat menghapus comment karena Anda bukan pemilik comment ini')
+    'DELETE_COMMENT.USER_IS_NOT_OWNER': new AuthorizationError('tidak dapat menghapus comment karena Anda bukan pemilik comment ini'),
+
+    // Replies
+    'NEW_REPLY.NOT_CONTAIN_NEEDED_PROPERTY': new InvariantError('tidak dapat membuat reply baru karena properti yang dibutuhkan tidak ada'),
+    'NEW_REPLY.NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('tidak dapat membuat reply baru karena tipe data tidak sesuai')
   }
 
   static translate (error): any {
