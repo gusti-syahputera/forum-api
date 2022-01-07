85a02885c1cd024c17fc6fd9c2ba5a9f1464561f
Gusti Ridhoni Syahputera <syahputera.doni@gmail.com>
2022-01-07 14:06:38 +0800

    Add AddedReply entity

---
+++ src/Domains/replies/entities/AddedReply.ts
@@ -0,0 +1,27 @@
+export default class AddedReply {
+  readonly id: string;
+  readonly content: string;
+  readonly owner: string;
+
+  constructor (payload) {
+    ({
+      id: this.id,
+      content: this.content,
+      owner: this.owner
+    } = this.verifyPayload(payload))
+  }
+
+  private verifyPayload (payload): { id: string, content: string, owner: string } {
+    const { id, content, owner } = payload
+
+    if (!id || !content || !owner) {
+      throw new Error('ADDED_REPLY.NOT_CONTAIN_NEEDED_PROPERTY')
+    }
+
+    if (typeof id !== 'string' || typeof content !== 'string' || typeof owner !== 'string') {
+      throw new Error('ADDED_REPLY.NOT_MEET_DATA_TYPE_SPECIFICATION')
+    }
+
+    return payload
+  }
+}
