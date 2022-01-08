293437e0acf5772e46004edc6948e2fff9305423
Gusti Ridhoni Syahputera <syahputera.doni@gmail.com>
2022-01-08 11:42:00 +0800

    Improve RefreshAuthenticationUseCase.test.ts

--- src/Applications/use_case/_test/RefreshAuthenticationUseCase.test.ts
+++ src/Applications/use_case/_test/RefreshAuthenticationUseCase.test.ts
@@ -1,85 +1,86 @@
+import * as faker from 'faker'
+import { createMock } from 'ts-auto-mock'
+import { method, On } from 'ts-auto-mock/extension'
+
 import AuthenticationRepository from '../../../Domains/authentications/AuthenticationRepository'
 import AuthenticationTokenManager from '../../security/AuthenticationTokenManager'
 import RefreshAuthenticationUseCase from '../RefreshAuthenticationUseCase'
 
-const instantAsyncFunc = async (): Promise<any> => await Promise.resolve()
-
-const getAuthenticationRepositoryMock = (): AuthenticationRepository => ({
-  addToken: jest.fn().mockImplementation(instantAsyncFunc),
-  checkAvailabilityToken: jest.fn().mockImplementation(instantAsyncFunc),
-  deleteToken: jest.fn().mockImplementation(instantAsyncFunc)
-})
-
-const getAuthenticationTokenManagerMock = (): AuthenticationTokenManager => ({
-  createAccessToken: jest.fn().mockImplementation(instantAsyncFunc),
-  createRefreshToken: jest.fn().mockImplementation(instantAsyncFunc),
-  decodePayload: jest.fn().mockImplementation(instantAsyncFunc),
-  verifyRefreshToken: jest.fn().mockImplementation(instantAsyncFunc)
-})
-
 describe('RefreshAuthenticationUseCase', () => {
-  it('should throw error if use case payload not contain refresh token', async () => {
-    // Arrange
+  it('should reject if payload does not contain refresh token', async () => {
+    // Arrange doubles
+    const authenticationRepository = createMock<AuthenticationRepository>()
+    const authenticationTokenManager = createMock<AuthenticationTokenManager>()
+
+    // Arrange input
     const useCasePayload = {}
-    const refreshAuthenticationUseCase = new RefreshAuthenticationUseCase(
-      getAuthenticationRepositoryMock(),
-      getAuthenticationTokenManagerMock()
-    )
-
-    // Action & Assert
-    await expect(refreshAuthenticationUseCase.execute(useCasePayload))
-      .rejects
-      .toThrowError('REFRESH_AUTHENTICATION_USE_CASE.NOT_CONTAIN_REFRESH_TOKEN')
-  })
-
-  it('should throw error if refresh token not string', async () => {
-    // Arrange
-    const useCasePayload = {
-      refreshToken: 1
-    }
-    const refreshAuthenticationUseCase = new RefreshAuthenticationUseCase(
-      getAuthenticationRepositoryMock(),
-      getAuthenticationTokenManagerMock()
-    )
-
-    // Action & Assert
-    await expect(refreshAuthenticationUseCase.execute(useCasePayload))
-      .rejects
-      .toThrowError('REFRESH_AUTHENTICATION_USE_CASE.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION')
-  })
-
-  it('should orchestrating the refresh authentication action correctly', async () => {
-    // Arrange
-    const useCasePayload = {
-      refreshToken: 'some_refresh_token'
-    }
-    const mockAuthenticationRepository = getAuthenticationRepositoryMock()
-    const mockAuthenticationTokenManager = getAuthenticationTokenManagerMock()
-
-    // Mocking
-    mockAuthenticationTokenManager.decodePayload = jest.fn()
-      .mockImplementation(async () => ({ username: 'dicoding', id: 'user-123' }))
-    mockAuthenticationTokenManager.createAccessToken = jest.fn()
-      .mockImplementation(async () => 'some_new_access_token')
-
-    // Create the use case instace
-    const refreshAuthenticationUseCase = new RefreshAuthenticationUseCase(
-      mockAuthenticationRepository,
-      mockAuthenticationTokenManager
-    )
 
     // Action
-    const accessToken = await refreshAuthenticationUseCase.execute(useCasePayload)
+    const useCase = new RefreshAuthenticationUseCase(authenticationRepository, authenticationTokenManager)
+    const promise = useCase.execute(useCasePayload)
 
     // Assert
-    expect(mockAuthenticationTokenManager.verifyRefreshToken)
+    await expect(promise).rejects.toThrowError('REFRESH_AUTHENTICATION_USE_CASE.NOT_CONTAIN_REFRESH_TOKEN')
+  })
+
+  it('should reject if refresh token is not string', async () => {
+    // Arrange doubles
+    const authenticationRepository = createMock<AuthenticationRepository>()
+    const authenticationTokenManager = createMock<AuthenticationTokenManager>()
+
+    // Arrange input
+    const useCasePayload = {
+      refreshToken: faker.datatype.number()
+    }
+
+    // Action
+    const useCase = new RefreshAuthenticationUseCase(authenticationRepository, authenticationTokenManager)
+    const promise = useCase.execute(useCasePayload)
+
+    // Assert
+    await expect(promise).rejects.toThrowError('REFRESH_AUTHENTICATION_USE_CASE.PAYLOAD_NOT_MEET_DATA_TYPE_SPECIFICATION')
+  })
+
+  it('should orchestrate the refresh authentication action correctly', async () => {
+    // Arrange doubles
+    const authenticationRepository = createMock<AuthenticationRepository>()
+    const authenticationRepositoryMocks = {
+      checkAvailabilityToken: On(authenticationRepository)
+        .get(method('checkAvailabilityToken'))
+        .mockImplementation(async () => await Promise.resolve())
+    }
+
+    const tokenPayload = {
+      username: faker.internet.userName(),
+      id: `user-${faker.datatype.uuid()}`
+    }
+    const accessToken = faker.datatype.hexaDecimal(64)
+    const authenticationTokenManager = createMock<AuthenticationTokenManager>()
+    const authenticationTokenManagerMocks = {
+      decodePayload: On(authenticationTokenManager)
+        .get(method('decodePayload'))
+        .mockResolvedValue(tokenPayload),
+      createAccessToken: On(authenticationTokenManager)
+        .get(method('createAccessToken'))
+        .mockResolvedValue(accessToken)
+    }
+
+    // Arrange input
+    const useCasePayload = {
+      refreshToken: faker.datatype.hexaDecimal(64)
+    }
+
+    // Action
+    const useCase = new RefreshAuthenticationUseCase(authenticationRepository, authenticationTokenManager)
+    const promise = useCase.execute(useCasePayload)
+
+    // Assert
+    await expect(promise).resolves.toStrictEqual(accessToken)
+    expect(authenticationRepositoryMocks.checkAvailabilityToken)
       .toBeCalledWith(useCasePayload.refreshToken)
-    expect(mockAuthenticationRepository.checkAvailabilityToken)
+    expect(authenticationTokenManagerMocks.decodePayload)
       .toBeCalledWith(useCasePayload.refreshToken)
-    expect(mockAuthenticationTokenManager.decodePayload)
-      .toBeCalledWith(useCasePayload.refreshToken)
-    expect(mockAuthenticationTokenManager.createAccessToken)
-      .toBeCalledWith({ username: 'dicoding', id: 'user-123' })
-    expect(accessToken).toEqual('some_new_access_token')
+    expect(authenticationTokenManagerMocks.createAccessToken)
+      .toBeCalledWith(tokenPayload)
   })
 })
