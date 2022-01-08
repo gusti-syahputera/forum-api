b908b6cbc28c2876e6281be1a572222b14bff5c5
Gusti Ridhoni Syahputera <syahputera.doni@gmail.com>
2022-01-08 12:02:52 +0800

    Improve LoginUserUseCase.test.ts

--- src/Applications/use_case/_test/LoginUserUseCase.test.ts
+++ src/Applications/use_case/_test/LoginUserUseCase.test.ts
@@ -1,3 +1,7 @@
+import * as faker from 'faker'
+import { createMock } from 'ts-auto-mock'
+import { method, On } from 'ts-auto-mock/extension'
+
 import { NewAuth } from '../../../Domains/authentications/entities'
 import UserRepository from '../../../Domains/users/UserRepository'
 import AuthenticationRepository from '../../../Domains/authentications/AuthenticationRepository'
@@ -5,67 +9,80 @@ import AuthenticationTokenManager from '../../security/AuthenticationTokenManage
 import PasswordHash from '../../security/PasswordHash'
 import LoginUserUseCase from '../LoginUserUseCase'
 
-const instantAsyncFunc = async (): Promise<any> => await Promise.resolve()
+describe('LoginUserUseCase', () => {
+  it('should orchestrate the get authentication action correctly', async () => {
+    // Arrange UserRepository's mock
+    const userRepository = createMock<UserRepository>()
+    const userId = `user-${faker.internet.userName()}`
+    const hashedPassword = faker.internet.password()
+    const userRepositoryMocks = {
+      getIdByUsername: On(userRepository)
+        .get(method('getIdByUsername'))
+        .mockReturnValue(userId),
+      getPasswordByUsername: On(userRepository)
+        .get(method('getPasswordByUsername'))
+        .mockResolvedValue(hashedPassword)
+    }
 
-describe('GetAuthenticationUseCase', () => {
-  it('should orchestrating the get authentication action correctly', async () => {
-    // Arrange
+    // Arrange AuthenticationRepository's mock
+    const authenticationRepository = createMock<AuthenticationRepository>()
+    const authenticationRepositoryMocks = {
+      addToken: On(authenticationRepository)
+        .get(method('addToken'))
+        .mockImplementation(async () => await Promise.resolve())
+    }
+
+    // Arrange AuthenticationTokenManager's mock
+    const accessToken = faker.datatype.hexaDecimal(64)
+    const refreshToken = faker.datatype.hexaDecimal(64)
+    const authenticationTokenManager = createMock<AuthenticationTokenManager>()
+    const authenticationTokenManagerMocks = {
+      createAccessToken: On(authenticationTokenManager)
+        .get(method('createAccessToken'))
+        .mockResolvedValue(accessToken),
+      createRefreshToken: On(authenticationTokenManager)
+        .get(method('createRefreshToken'))
+        .mockResolvedValue(refreshToken)
+    }
+
+    // Arrange PasswordHash's mock
+    const passwordHash = createMock<PasswordHash>()
+    const passwordHashMocks = {
+      comparePassword: On(passwordHash)
+        .get(method('comparePassword'))
+        .mockImplementation(async () => await Promise.resolve())
+    }
+
+    // Arrange input
     const useCasePayload = {
-      username: 'dicoding',
-      password: 'secret'
+      username: faker.internet.userName(),
+      password: faker.internet.password()
     }
-    const expectedAuthentication = new NewAuth({
-      accessToken: 'access_token',
-      refreshToken: 'refresh_token'
-    })
-
-    // Mocked dependencies
-    const mockUserRepository: UserRepository = {
-      addUser: jest.fn().mockImplementation(instantAsyncFunc),
-      getIdByUsername: jest.fn().mockImplementation(async () => 'user-123'),
-      getPasswordByUsername: jest.fn().mockImplementation(async () => 'encrypted_password'),
-      verifyAvailableUsername: jest.fn().mockImplementation(instantAsyncFunc)
-    }
-    const mockAuthenticationRepository: AuthenticationRepository = {
-      addToken: jest.fn().mockImplementation(async () => await Promise.resolve()),
-      checkAvailabilityToken: jest.fn().mockImplementation(instantAsyncFunc),
-      deleteToken: jest.fn().mockImplementation(instantAsyncFunc)
-    }
-    const mockAuthenticationTokenManager: AuthenticationTokenManager = {
-      createAccessToken: jest.fn().mockImplementation(async () => expectedAuthentication.accessToken),
-      createRefreshToken: jest.fn().mockImplementation(async () => expectedAuthentication.refreshToken),
-      decodePayload: jest.fn().mockImplementation(instantAsyncFunc),
-      verifyRefreshToken: jest.fn().mockImplementation(instantAsyncFunc)
-    }
-    const mockPasswordHash: PasswordHash = {
-      comparePassword: jest.fn().mockImplementation(instantAsyncFunc),
-      hash: jest.fn().mockImplementation(instantAsyncFunc)
-    }
-
-    // create use case instance
-    const loginUserUseCase = new LoginUserUseCase(
-      mockUserRepository,
-      mockAuthenticationRepository,
-      mockAuthenticationTokenManager,
-      mockPasswordHash
-    )
 
     // Action
-    const actualAuthentication = await loginUserUseCase.execute(useCasePayload)
+    const useCase = new LoginUserUseCase(userRepository, authenticationRepository, authenticationTokenManager, passwordHash)
+    const promise = useCase.execute(useCasePayload)
 
     // Assert
-    expect(actualAuthentication).toEqual(expectedAuthentication)
-    expect(mockUserRepository.getPasswordByUsername)
-      .toBeCalledWith('dicoding')
-    expect(mockPasswordHash.comparePassword)
-      .toBeCalledWith('secret', 'encrypted_password')
-    expect(mockUserRepository.getIdByUsername)
-      .toBeCalledWith('dicoding')
-    expect(mockAuthenticationTokenManager.createAccessToken)
-      .toBeCalledWith({ username: 'dicoding', sub: 'user-123' })
-    expect(mockAuthenticationTokenManager.createRefreshToken)
-      .toBeCalledWith({ username: 'dicoding', sub: 'user-123' })
-    expect(mockAuthenticationRepository.addToken)
+    const expectedAuthentication = new NewAuth({ accessToken, refreshToken })
+    await expect(promise).resolves.toMatchObject(expectedAuthentication)
+    expect(userRepositoryMocks.getPasswordByUsername)
+      .toBeCalledWith(useCasePayload.username)
+    expect(passwordHashMocks.comparePassword)
+      .toBeCalledWith(useCasePayload.password, hashedPassword)
+    expect(userRepositoryMocks.getIdByUsername)
+      .toBeCalledWith(useCasePayload.username)
+
+    const expectedTokenPayload = {
+      username: useCasePayload.username,
+      sub: userId
+    }
+    expect(authenticationTokenManagerMocks.createAccessToken)
+      .toBeCalledWith(expectedTokenPayload)
+    expect(authenticationTokenManagerMocks.createRefreshToken)
+      .toBeCalledWith(expectedTokenPayload)
+
+    expect(authenticationRepositoryMocks.addToken)
       .toBeCalledWith(expectedAuthentication.refreshToken)
   })
 })
